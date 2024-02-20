ff8b94ce6d63941335e9107185802d324e5c5cc3
seheichenko-anna <seheichenko.anna@gmail.com>
2024-02-20 19:14:23 +0200

    Update Modal

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,20 +1,33 @@
-import React from 'react';
 import { Overlay, ModalWindow } from './Modal.styled';
+import React, { Component } from 'react';
 
-const Modal = ({ largeImageURL, onClose }) => {
-  const checkForCloseModal = e => {
-    if (e.target === e.currentTarget) {
-      onClose();
+export default class Modal extends Component {
+  componentDidMount() {
+    document.addEventListener('keydown', this.handleKeyDown);
+  }
+  componentWillUnmount() {
+    document.removeEventListener('keydown', this.handleKeyDown);
+  }
+
+  handleKeyDown = e => {
+    if (e.key === 'Escape') {
+      this.props.onClose();
     }
   };
 
-  return (
-    <Overlay onClick={checkForCloseModal}>
-      <ModalWindow>
-        <img src={largeImageURL} alt="" />
-      </ModalWindow>
-    </Overlay>
-  );
-};
+  checkForCloseModal = e => {
+    if (e.target === e.currentTarget) {
+      this.props.onClose();
+    }
+  };
 
-export default Modal;
+  render() {
+    return (
+      <Overlay onClick={this.checkForCloseModal}>
+        <ModalWindow>
+          <img src={this.props.largeImageURL} alt="" />
+        </ModalWindow>
+      </Overlay>
+    );
+  }
+}
